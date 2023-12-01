752b54c9e45618586e9ff4e139b1d70316c0e4d3
JuraRusan <rusanjura@gmail.com>
2023-12-01 17:22:04 +0200

    fix directory

--- react-app/src/components/NameFormat/NameFormat.js
+++
@@ -1,5 +0,0 @@
-function NameFormat(requirement) {
-  return requirement.replace(/_/g, ' ').charAt(0).toUpperCase() + requirement.replace(/_/g, ' ').slice(1);
-}
-
-export default NameFormat;
--- react-app/src/components/roundToMultiple/RoundToMultiple.js
+++
@@ -1,9 +0,0 @@
-export default function roundToMultiple(number, multiple) {
-  let result;
-  if (number % multiple === 0) {
-    result = number
-  } else {
-    result = Math.ceil(number / multiple) * multiple
-  }
-  return result;
-}
