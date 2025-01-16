a758e8d5a332585bf829b5f60f57e744b2790f86
JuraRusan <rusanjura@gmail.com>
2025-01-16 19:32:11 +0200

    add ver

--- src/main/Main.js
+++ src/main/Main.js
@@ -7,6 +7,7 @@ import styles from "./Main.module.scss";
 const Main = () => {
   return (
     <div className={classNames(styles["main_wrapper"])}>
+      <label className={classNames(styles["ver"])}>ver. Minecraft 1.21.4</label>
       <CraftCalculator />
     </div>
   );
