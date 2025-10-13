b9fbae3e05d4eafac978b3a5849b67f358e8145d
JuraRusan <rusanjura@gmail.com>
2025-10-13 19:43:47 +0300

    update 1.21.9/10

--- src/main/Main.js
+++ src/main/Main.js
@@ -6,7 +6,7 @@ import styles from "./Main.module.scss";
 const Main = () => {
   return (
     <div className={styles["main_wrapper"]}>
-      <label className={styles["ver"]}>ver. Minecraft 1.21.8</label>
+      <label className={styles["ver"]}>ver. Minecraft 1.21.9/10</label>
       <CraftCalculator />
     </div>
   );
